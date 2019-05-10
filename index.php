819fb08dce75dfd0440ff6652138148b008b1541
jc-t-od <takeru@pacbell.net>
2019-05-09 22:23:22 -0700

    Initial index

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Home of The Doormats | Music from San Francisco</title>
+        <style>
+            body {
+                background-color: #000;
+                color: #fff;
+                font-family: "Lucida Console", Monaco, monospace;
+            }
+            a:link {
+                color: #fff;
+            }
+            a:visited {
+              color: #ddd;
+            }
+            .center {
+                text-align: center;
+            }
+        </style>
+        <link rel="icon"
+          type="image/png"
+          href="http://www.thedoormats.com/feet-favicon.png">
+        <link rel="apple-touch-icon" href="http://www.thedoormats.com/apple-touch.png" sizes="192x192">
+    </head>
+    <body>
+        <div class="center">
+            <img src="assets/img/coasters-logo-600.jpg">
+        </div>
+        <div class="center">
+            <a href="https://thedoormats.bandcamp.com/" target="_blank">get music</a>
+        </div>
+    </body>
+</html>
